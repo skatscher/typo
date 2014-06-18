6ffa5133430f2924127b26d219ec8975f121697b
Stefan Katscher <skatscher@googlemail.com>
2014-06-17 19:08:51 -0700

    try to fix bug

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -6,13 +6,8 @@ class Admin::CategoriesController < Admin::BaseController
 
   def new 
     respond_to do |format|
-      format.html {
-        @category = Category.new
-        new_or_edit
-      }
-      format.js {
-        @category = Category.new
-      }
+      format.html { new_or_edit }
+      format.js { @category = Category.new }
     end
   end
 
@@ -28,7 +23,7 @@ class Admin::CategoriesController < Admin::BaseController
 
   def new_or_edit
     @categories = Category.find(:all)
-    @category ||= Category.find(params[:id])
+    @category = params[:id] ? Category.new : Category.find(params[:id])
     @category.attributes = params[:category]
     if request.post?
       respond_to do |format|
